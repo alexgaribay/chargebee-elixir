d3abd242cd597b8435e5f588ceded925ff653fe1
Alex Garibay <alex@alexgaribay.com>
2017-02-26 22:42:01 -0600

    Update mix config

--- mix.exs
+++ mix.exs
@@ -4,19 +4,23 @@ defmodule Chargebee.Mixfile do
   def project do
     [app: :chargebee,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
      description: "Elixir wrapper for Chargebee API",
-     package: package]
+     package: package()]
   end
 
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:httpoison, :logger, :poison]]
+    [
+      extra_applications: [
+        :logger
+      ]
+    ]
   end
 
   # Dependencies can be Hex packages:
@@ -30,17 +34,15 @@ defmodule Chargebee.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:httpoison, "~> 0.9"},
-      {:poison, "~> 2.0"}
+      {:httpoison, "~> 0.11"},
+      {:poison, "~> 3.1.0"}
     ]
   end
 
   defp package do
     [ # These are the default files included in the package
       files: ["lib", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
-      contributors: ["Stuart Eccles"],
       licenses: ["MIT"],
-      links: %{ "GitHub" => "https://github.com/stueccles/chargebee-elixir" }
     ]
   end
 end
